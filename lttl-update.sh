52a1a9729fa8545eeae1e5f9f95c1eda082a6fff
Dmitry Dyugaev <dyugaev@gmail.com>
2023-08-05 17:59:00 +0300

    Dev branch testing

--- lttl-update.sh
+++ lttl-update.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 script_folder=/opt/longterm-timelapse
+branch=$(git branch --show-current)
 
 function update {
     local a=${1%%.*} b=${2%%.*}
     if [[ "10#${a:-0}" -lt "10#${b:-0}" ]]; then
+        echo "Updating the script"
         cd $script_folder
         git remote prune origin
         git fetch origin
@@ -15,5 +17,5 @@ function update {
 }
 
 current_version=$(cat $script_folder/version.txt)
-online_version=$(curl -s https://raw.githubusercontent.com/ddyugaev/longterm-timelapse/main/version.txt)
+online_version=$(curl -s https://raw.githubusercontent.com/ddyugaev/longterm-timelapse/${branch}/version.txt)
 update $current_version $online_version
